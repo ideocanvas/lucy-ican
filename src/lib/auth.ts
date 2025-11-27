19e557b7dd08439cf3755d2c7e1ccbee8a9fdd33
Thomas Tse <thomas_tse@nmt.com.hk>
2025-11-27 16:34:01 +0800

    add better auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,15 @@
+import { PrismaClient } from "@prisma/client/edge";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+import { username } from "better-auth/plugins";
+
+const prisma = new PrismaClient();
+export const auth = betterAuth({
+  database: prismaAdapter(prisma, {
+    provider: "postgresql", // or "mysql", "postgresql", ...etc
+  }),
+  emailAndPassword: {
+    enabled: true,
+  },
+  plugins: [username()],
+});
